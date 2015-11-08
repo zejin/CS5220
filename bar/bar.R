cb3b3f26851800a9411318bf0d5d9b3ffa16dbb3
zj58 <zj58@dss1.cac.cornell.edu>
2015-11-08 17:35:49 -0500

    update OpenMP

--- bar/bar.R
+++ bar/bar.R
@@ -1,16 +1,2 @@
-dyn.load("bar.so")
-
-bar <- function(x) {
-   if (!is.numeric(x))
-      stop("argument x must be numeric")
-   out <- .C("bar",
-             n=as.integer(length(x)),
-             x=as.double(x))
-   return(out$x)
-}
-
-x <- rnorm(10)
-
-x2 <- bar(x)
-
-identical(x^2, x2)
+dyn.load('bar.so')
+.Call('convolve2', 1:10, 1:10)
--- bar/bar.c
+++ bar/bar.c
@@ -1,9 +1,37 @@
-void bar(int *nin, double *x)
+#include <stdio.h>
+#include <R.h>
+#include <Rinternals.h>
+#include <omp.h>
+
+SEXP convolve2(SEXP a, SEXP b) 
 {
-  int n = nin[0];
+  int na, nb, nab;
+  double *xa, *xb, *xab;
+  SEXP ab;
 
-  int i;
+  a = PROTECT(coerceVector(a, REALSXP));
+  b = PROTECT(coerceVector(b, REALSXP));
+  na = length(a); 
+  nb = length(b); 
+  nab = na + nb - 1;
+  ab = PROTECT(allocVector(REALSXP, nab));
+    
+  xa = REAL(a); 
+  xb = REAL(b); 
+  xab = REAL(ab);
 
-  for (i=0; i<n; i++)
-    x[i] = x[i] * x[i];
+  for (int i = 0; i < nab; i++) 
+    xab[i] = 0.0;
+
+  int np = omp_get_num_procs();
+  omp_set_num_threads(np);//set number of threads   
+  printf("number of threads: %d\n", np);
+
+  #pragma omp parallel for
+  for (int i = 0; i < na; i++)
+    for (int j = 0; j < nb; j++) 
+      xab[i + j] += xa[i] * xb[j];
+    
+  UNPROTECT(3);
+  return ab;
 }
